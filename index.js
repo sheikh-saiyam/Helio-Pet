e9dc3bc3f36b5f7fbe3513a322b05dcebaa4354a
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-05-09 14:00:24 +0600

    Add To Cart Functionality Added

--- index.js
+++ index.js
@@ -1,33 +1,76 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
 function openDrawer() {
   document.getElementById("cartDrawer").classList.add("open");
 }
 
 function closeDrawer() {
-//   document.getElementById("cartDrawer").classList.remove("open");
+  document.getElementById("cartDrawer").classList.remove("open");
 }
 
 function addToCart() {
+  const cartItem = {
+    id: 1,
+    product_title: "Helio Pet Device",
+    price: 249,
+    quantity: 1,
+  };
+
+  const isItemExitsInCart = cart.find((i) => i.id === 1);
+
+  if (isItemExitsInCart) {
+    if (isItemExitsInCart.quantity < 10) isItemExitsInCart.quantity += 1;
+  } else {
+    cart.push(cartItem);
+  }
+
+  localStorage.setItem("cart", JSON.stringify(cart));
   openDrawer();
+  renderCart();
+}
+
+function updateQuantity(mehtod) {
+  if (mehtod === "increase") cart[0].quantity += 1;
+  else cart[0].quantity -= 1;
+  localStorage.setItem("cart", JSON.stringify(cart));
   renderCart();
 }
 
 function renderCart() {
   const container = document.getElementById("cartItems");
   container.innerHTML = "";
-
-  //   cart.forEach((item) => {
-  const div = document.createElement("div");
-  div.className = "cart-item";
-  div.innerHTML = `
-          <strong>${"item.name"}</strong><br>
-          <p>Price: $${"item.sellingPrice * item.quantity"} <span class="line-through">$${"   item.comparePrice * item.quantity"}</span></p>
-          <button onclick="changeQty(${"item.id"}, -1)">-</button>
-          ${"item.quantity"}
-          <button onclick="changeQty(${"item.id"}, 1)">+</button>
-          <button onclick="removeFromCart(${"         item.id"})" style="color:red;">🗑️</button>
-        `;
-  container.appendChild(div);
-  //   });
+  cart.forEach((item) => {
+    const div = document.createElement("div");
+    div.className = "cart-item";
+    div.innerHTML = `   
+       <!-- header -->
+        <div class="cart_header">
+          <h4>Product</h4>
+          <h4>Total</h4>
+        </div>
+        <!-- products -->
+        <div class="product_container">
+          <!-- image -->
+          <div class="product_img">
+            <img
+              src="./images/product pic variations.png"
+              alt="product image"
+            />
+          </div>
+          <!-- content -->
+          <div class="product_details">
+            <h6>${item.product_title}</h6>
+            <h6>$${item.price}</h6>
+            <!-- quantity selector -->
+            <div class="quantity_selector">
+              <div style="font-weight: bold; cursor: pointer" onclick={updateQuantity('decrease')}>-</div>
+              <div>${item.quantity}</div>
+              <div style="font-weight: bold; cursor: pointer" onclick={updateQuantity('increase')}>+</div>
+            </div>
+          </div>
+        </div>`;
+    container.appendChild(div);
+  });
 }
 
 renderCart();
